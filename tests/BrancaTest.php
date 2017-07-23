6b2eafb6872acce4ee8367693188ee89f1860f94
Mika Tuupola <tuupola@appelsiini.net>
2017-07-23 23:21:58 +0300

    Sync test vector 2 with JavaScript version

--- tests/BrancaTest.php
+++ tests/BrancaTest.php
@@ -45,6 +45,20 @@ class BrancaTest extends TestCase
         $this->assertEquals("Hello world!", $decoded);
     }
 
+    public function testShouldPassTestVector2()
+    {
+        $this->expectException(\RuntimeException::class);
+
+        $key = "supersecretkeyyoushouldnotcommit";
+        $nonce = hex2bin("0102030405060708090a0b0c0102030405060708090a0b0c");
+        $timestamp = 123206400;
+
+        $branca = new Branca($key);
+        NSA::setProperty($branca, "nonce", $nonce);
+        $token = $branca->encode("Hello world!", $timestamp);
+        $decoded = $branca->decode($token, 3600);
+    }
+
     public function testShouldEncodeAndDecode()
     {
         $key = "supersecretkeyyoushouldnotcommit";
@@ -81,15 +95,5 @@ class BrancaTest extends TestCase
         $branca = new Branca($key);
         $token = $branca->encode("Hello world!");
         $decoded = $branca->decode("XX{$token}XX");
-    }
-
-    public function testShouldThrowExpiredToken()
-    {
-        $this->expectException(\RuntimeException::class);
-        $key = "supersecretkeyyoushouldnotcommit";
-        $branca = new Branca($key);
-        $token = $branca->encode("Hello world!");
-        sleep(2);
-        $decoded = $branca->decode($token, 1);
     }
 }
